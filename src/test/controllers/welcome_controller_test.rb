6cfb9ce2bf84d57afa6195a876541d499fc26b96
macbook <brian.dawson98@gmail.com>
2019-10-03 15:42:14 -0400

    Fixxed

--- src/test/controllers/welcome_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class WelcomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get buttons" do
-    get welcome_buttons_url
-    assert_response :success
-  end
-
-end
